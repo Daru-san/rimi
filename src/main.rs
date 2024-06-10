28374c6f3b1f9aafab9533cb29b473a02bff3919
Daru <mica.tui@proton.me>
2024-06-10 17:14:06 +0200

    add sources

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+use std::process::Command;
+
+#[derive(Parser, Debug)]
+#[command(version,about,long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    source_image: String,
+
+    #[arg(short, long)]
+    final_image: String,
+}
+fn main() {
+    let args = Cli::parse();
+
+    Command::new("convert")
+        .arg(args.source_image)
+        .arg(args.final_image)
+        .spawn()
+        .expect("ls command failed to start");
+}
