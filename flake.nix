32e68fa1995c959f9a69f3444e019d1b9af65a44
Daru <mica.tui@proton.me>
2024-06-10 17:55:54 +0200

    clean comments

--- flake.nix
+++ flake.nix
@@ -18,21 +18,13 @@
         toolchain = pkgs.rustPlatform;
       in rec
       {
-        # Executed by `nix build`
         packages.default = toolchain.buildRustPackage {
           pname = "r-pg";
           version = "0.1.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
-
-          # For other makeRustPlatform features see:
-          # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
         };
-
-        # Executed by `nix run`
         apps.default = utils.lib.mkApp {drv = packages.default;};
-
-        # Used by `nix develop`
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             (with toolchain; [
@@ -44,8 +36,6 @@
             rustfmt
             pkg-config
           ];
-
-          # Specify the rust-src path (many editors rely on this)
           RUST_SRC_PATH = "${toolchain.rustLibSrc}";
         };
       }
