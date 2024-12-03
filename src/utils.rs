5cff8d8148a4da1b42996394c358a14de418c889
Daru <mica.tui@proton.me>
2024-12-03 16:41:45 +0200

    src/utils: image saving with formatting

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,50 @@
+use image::imageops::FilterType;
+use image::{self, DynamicImage, ImageFormat};
+use std::process::exit;
 
 pub fn save_image(image: &DynamicImage, path: &str) {
     image.save(path).expect("File save error:");
+}
+
+pub fn save_image_format(image: DynamicImage, path: &str, format: Option<String>) {
+    if format.is_some() {
+        let mut img_format = ImageFormat::Png;
+        let local_format: &str = &format
+            .map(|s| s.to_string())
+            .expect("Something strange happened");
+
+        match local_format.to_uppercase().as_str() {
+            "PNG" => {
+                img_format = ImageFormat::Png;
+            }
+            "JPG" => {
+                img_format = ImageFormat::Jpeg;
+            }
+            "JPEG" => {
+                img_format = ImageFormat::Jpeg;
+            }
+            "WEBP" => {
+                img_format = ImageFormat::WebP;
+            }
+            "ICO" => {
+                img_format = ImageFormat::Ico;
+            }
+            "GIF" => {
+                img_format = ImageFormat::Gif;
+            }
+            "AVIF" => {
+                img_format = ImageFormat::Avif;
+            }
+            _ => {
+                let _ = img_format;
+                eprintln!("Please choose one of png, jpg, jpeg, webp, ico, gif or avif.");
+                exit(0);
+            }
+        };
+        image
+            .save_with_format(path, img_format)
+            .expect("File save error:");
+    } else {
+        image.save(path).expect("File save error:");
+    }
 }
