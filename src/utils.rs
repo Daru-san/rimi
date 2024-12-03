28a2df1e991001a399b621ca131e9cf8d734b4be
Daru <mica.tui@proton.me>
2024-12-03 16:53:23 +0200

    src/utils: implement image resizing

--- src/utils.rs
+++ src/utils.rs
@@ -57,3 +57,37 @@ pub fn save_image_format(image: DynamicImage, out: &str, format: Option<String>)
         image.save(path).expect("File save error:");
     }
 }
+
+#[derive(Debug, Default)]
+pub struct Dimensions {
+    pub x: u32,
+    pub y: u32,
+}
+
+pub fn resize_image(image: &mut DynamicImage, dimensions: Dimensions, filter: String) {
+    let mut filter_type = FilterType::Nearest;
+    let local_filter: &str = &filter;
+    match local_filter.to_uppercase().as_str() {
+        "NEAREST" => {
+            filter_type = FilterType::Nearest;
+        }
+        "TRIANGLE" => {
+            filter_type = FilterType::Triangle;
+        }
+        "GAUSSIAN" => {
+            filter_type = FilterType::Gaussian;
+        }
+        "CATMULLROM" => {
+            filter_type = FilterType::CatmullRom;
+        }
+        "LANCZOS3" => {
+            filter_type = FilterType::Lanczos3;
+        }
+        _ => {
+            let _ = filter_type;
+            eprintln!("Please pick a valid filter out of: Nearest, Triangle, Gaussian, CatmullRom and Lanczos3");
+            exit(0);
+        }
+    }
+    image.resize(dimensions.x, dimensions.y, filter_type);
+}
