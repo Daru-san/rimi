e91ecc26b7e01737ba565b7f5e9aa142491cf7a0
Daru <mica.tui@proton.me>
2024-12-05 20:11:28 +0200

    refactor: set image path and directory as command-specific arguments

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,8 @@ struct Args {
     #[command(subcommand)]
     cmd: Option<Commands>,
 
-    /// Path to the input image
-    filename: String,
-
-    /// Path where the saved image should be written
-    #[clap(short, long,requires_all = ["filename"])]
-    output: Option<String>,
-
     /// Overwrite any existing files when saving the image
-    #[clap(short = 'x', long, default_value = "false")]
+    #[clap(short = 'x', long, default_value = "false", global = true)]
     overwrite: bool,
 }
 
@@ -35,11 +28,23 @@ enum Commands {
         /// Format of the new image.
         #[clap(short, long)]
         format: Option<String>,
+        /// Path to the input image
+        image_file: PathBuf,
+
+        /// Path where the saved image should be written
+        #[clap(short, long, global = true)]
+        output: Option<String>,
     },
 
     /// Resize an image
     #[clap(short_flag = 'r')]
     Resize {
+        /// Path to the image file
+        image_file: PathBuf,
+
+        /// Path where the saved image should be written
+        #[clap(short, long, global = true)]
+        output: Option<String>,
         /// New width
         #[clap(short)]
         x: u32,
@@ -62,6 +67,8 @@ enum Commands {
     Info {
         #[clap(short, long)]
         short: bool,
+        ///Path to the image file
+        image_file: PathBuf,
     },
 
     /// Batch image conversion
@@ -73,48 +80,49 @@ enum Commands {
 
 fn main() {
     let args = Args::parse();
-    let (infile, outfile) = (&args.filename, &args.output);
 
     let do_overwrite = &args.overwrite;
 
-    assert!(
-        Path::new(infile).exists(),
-        "File {} does not exist!",
-        infile
-    );
-
-    let output_file = if outfile.is_some() {
-        let result: String = outfile.clone().map(|s| s.to_string()).unwrap();
-        result
-    } else {
-        infile.to_string()
-    };
-
-    let mut image = image::ImageReader::open(infile)
-        .expect("Error opening image: ")
-        .decode()
-        .expect("Error decoding image: ");
-
     match &args.cmd {
-        Some(Commands::Convert { format }) => {
-            save_image_format(&image, &output_file, format.as_deref(), *do_overwrite);
+        Some(Commands::Convert {
+            format,
+            image_file,
+            output,
+        }) => {
+            let output_path = match output {
+                Some(e) => e.as_str(),
+                None => image_file
+                    .as_os_str()
+                    .to_str()
+                    .expect("Error parsing image path: "),
+            };
+            save_image_format(&image, output_path, format.as_deref(), *do_overwrite);
         }
         Some(Commands::Resize {
             x,
             y,
             filter,
             preserve_aspect,
+            image_file,
         }) => {
+            let output_path = match output {
+                Some(e) => e.as_str(),
+                None => image_file
+                    .as_os_str()
+                    .to_str()
+                    .expect("Error parsing image path: "),
+            };
             resize_image(
                 &mut image,
                 Dimensions { x: *x, y: *y },
                 filter.to_string(),
                 *preserve_aspect,
             );
-            save_image_format(&image, &output_file, None, *do_overwrite);
+            save_image_format(&image, output_path, None, *do_overwrite);
         }
-        Some(Commands::Info { short }) => {
-            print_info(&image, infile, *short);
+        Some(Commands::Info { short, image_file }) => {
+            let image = open_image(image_file.into());
+            print_info(&image, image_file.to_path_buf(), *short);
         }
         Some(Commands::Batch { images }) => {
             let images_str = images.iter().map(|s| s.as_str()).collect();
