6591454102fffae2e0794045e5a76c83f7d68eec
Daru <mica.tui@proton.me>
2024-12-06 22:59:34 +0200

    feat: implement app args
    Finally got the application arguments. Almost there!

---
+++ src/app.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+
+use clap::Parser;
+mod command;
+use command::Command;
+
+/// Simple image manipulation tool
+#[derive(Parser)]
+#[command(version,about,long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    pub cmd: Command,
+
+    #[clap(flatten)]
+    global_args: GlobalArgs,
+}
+
+#[derive(Parser)]
+pub struct GlobalArgs {
+    /// Overwrite any existing files when saving the image
+    #[clap(short = 'x', long, default_value = "false", global = true)]
+    pub overwrite: bool,
+}
+
+impl Args {
+    pub fn run(self) -> Result<(), Box<dyn Error>> {
+        self.cmd.run(&self.global_args)?;
+        Ok(())
+    }
+}
