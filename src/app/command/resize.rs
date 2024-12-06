f2007d2f8fd5936831140f9f5cc4c8cff63bd637
Daru <mica.tui@proton.me>
2024-12-06 21:13:37 +0200

    refactor: re-implement resize module

---
+++ src/app/command/resize.rs
@@ -0,0 +1,55 @@
+use crate::app::Args;
+use crate::utils::image::{open_image, resize_image, save_image_format, Dimensions};
+use clap::Parser;
+use std::error::Error;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+pub struct ResizeArgs {
+    /// Path to the image file
+    image_file: PathBuf,
+
+    /// Path where the saved image should be written
+    #[clap(short, long, global = true)]
+    output: Option<String>,
+
+    /// New width
+    width: u32,
+
+    /// New height
+    height: u32,
+
+    /// Image Sampling filter
+    #[clap(short, long, default_value = "Nearest")]
+    filter: String,
+
+    /// Preserve aspect ratio
+    #[clap(short, long)]
+    preserve_aspect: bool,
+}
+
+impl ResizeArgs {
+    pub fn run(&self, app_args: Args) -> Result<(), Box<dyn Error>> {
+        let mut image = open_image(self.image_file.clone())?;
+
+        let output_path = match &self.output {
+            Some(e) => e.as_str(),
+            None => self
+                .image_file
+                .as_os_str()
+                .to_str()
+                .expect("Error parsing image path: "),
+        };
+        resize_image(
+            &mut image,
+            Dimensions {
+                x: self.width,
+                y: self.height,
+            },
+            self.filter.to_string(),
+            self.preserve_aspect,
+        );
+        save_image_format(&image, output_path, None, app_args.overwrite);
+        Ok(())
+    }
+}
