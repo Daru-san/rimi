e6425ae79b426e996ae43c1b496333251786f013
Daru <mica.tui@proton.me>
2024-12-10 08:55:37 +0200

    refactor: recolor does not save images, remove image flags

--- src/app/command/recolor.rs
+++ src/app/command/recolor.rs
@@ -1,44 +1,18 @@
-use crate::{
-    app,
-    utils::{
-        color::ColorInfo,
-        image::{open_image, save_image_format},
-    },
-};
+use crate::utils::color::ColorInfo;
 use clap::Parser;
+use image::DynamicImage;
 use std::error::Error;
-use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 pub struct RecolorArgs {
-    /// Path to the input image
-    image_file: PathBuf,
-
-    /// Path where the saved image should be written
-    #[clap(short, long, global = true)]
-    output: Option<String>,
-
     /// Color type
     #[clap(short, long)]
     color_type: ColorInfo,
 }
 
 impl RecolorArgs {
-    pub fn run(&self, app_args: &app::GlobalArgs) -> Result<(), Box<dyn Error>> {
-        let mut image = open_image(self.image_file.clone())?;
-
-        let output_path = match &self.output {
-            Some(e) => e.as_str(),
-            None => self
-                .image_file
-                .as_os_str()
-                .to_str()
-                .expect("Error parsing image path: "),
-        };
-
-        self.color_type.convert_image(&mut image);
-
-        save_image_format(&image, output_path, None, app_args.overwrite)?;
+    pub fn run(&self, image: &mut DynamicImage) -> Result<(), Box<dyn Error>> {
+        self.color_type.convert_image(image);
         Ok(())
     }
 }
