825869e2171860966c6818b9fa05838b2f00670d
Daru <mica.tui@proton.me>
2024-12-10 09:33:18 +0200

    refactor: do not check images before transparentizing
    Will replace with a better check

--- src/app/command/transparent.rs
+++ src/app/command/transparent.rs
@@ -1,21 +1,13 @@
 use crate::utils::image::remove_background;
 use clap::Parser;
-use image::{DynamicImage, ImageFormat};
+use image::DynamicImage;
 use std::error::Error;
-use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 pub struct TransparentArgs {}
 
 impl TransparentArgs {
-    pub fn run(
-        &self,
-        image: &mut DynamicImage,
-        image_path: &PathBuf,
-    ) -> Result<(), Box<dyn Error>> {
-        if ImageFormat::from_path(image_path).unwrap() != ImageFormat::Png {
-            return Err(format!("{:?}: Image must be a png file.", image_path.as_os_str()).into());
-        }
+    pub fn run(&self, image: &mut DynamicImage) -> Result<(), Box<dyn Error>> {
         remove_background(image);
         Ok(())
     }
