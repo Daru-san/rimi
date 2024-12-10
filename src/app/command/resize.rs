22d933977d14f174f70ca66963576bb94f572bac
Daru <mica.tui@proton.me>
2024-12-10 08:55:56 +0200

    refactor: resize does not save images, remove path args

--- src/app/command/resize.rs
+++ src/app/command/resize.rs
@@ -1,18 +1,10 @@
-use crate::app;
-use crate::utils::image::{open_image, resize_image, save_image_format, Dimensions};
+use crate::utils::image::{resize_image, Dimensions};
 use clap::Parser;
+use image::DynamicImage;
 use std::error::Error;
-use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 pub struct ResizeArgs {
-    /// Path to the image file
-    image_file: PathBuf,
-
-    /// Path where the saved image should be written
-    #[clap(short, long, global = true)]
-    output: Option<String>,
-
     /// New width
     width: u32,
 
@@ -29,19 +21,9 @@ pub struct ResizeArgs {
 }
 
 impl ResizeArgs {
-    pub fn run(&self, app_args: &app::GlobalArgs) -> Result<(), Box<dyn Error>> {
-        let mut image = open_image(self.image_file.clone())?;
-
-        let output_path = match &self.output {
-            Some(e) => e.as_str(),
-            None => self
-                .image_file
-                .as_os_str()
-                .to_str()
-                .expect("Error parsing image path: "),
-        };
+    pub fn run(&self, image: &mut DynamicImage) -> Result<(), Box<dyn Error>> {
         resize_image(
-            &mut image,
+            image,
             Dimensions {
                 x: self.width,
                 y: self.height,
@@ -49,7 +31,6 @@ impl ResizeArgs {
             self.filter.to_string(),
             self.preserve_aspect,
         )?;
-        save_image_format(&image, output_path, None, app_args.overwrite)?;
         Ok(())
     }
 }
