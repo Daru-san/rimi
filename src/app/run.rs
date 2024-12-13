e81d051b5a14fca619cb8d72701576fd828b633d
Daru <mica.tui@proton.me>
2024-12-13 11:26:16 +0200

    refactor: pass subcommand flags via Run traits

--- src/app/run.rs
+++ src/app/run.rs
@@ -1,10 +1,12 @@
+use super::command::ImageCommand;
+
 mod batch;
 mod single;
 
 pub trait RunSingle {
-    fn run_single(&self) -> anyhow::Result<()>;
+    fn run_single(&self, command: &ImageCommand, verbosity: u32) -> anyhow::Result<()>;
 }
 
 pub trait RunBatch {
-    fn run_batch(&self) -> anyhow::Result<()>;
+    fn run_batch(&self, command: &ImageCommand, verbosity: u32) -> anyhow::Result<()>;
 }
