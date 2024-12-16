53b692d52cd7af13e973f4f41d825fd7c5b76f74
Daru <mica.tui@proton.me>
2024-12-16 08:19:24 +0200

    refactor: account for image formats when creating paths

--- src/backend/paths.rs
+++ src/backend/paths.rs
@@ -4,51 +4,60 @@ pub fn create_paths(
     files: Vec<PathBuf>,
     destination: PathBuf,
     name_expr: Option<&str>,
+    image_format: Option<&str>,
 ) -> Result<Vec<PathBuf>, String> {
     let mut paths: Vec<PathBuf> = Vec::new();
-    let dest_dir = destination;
 
-    if !dest_dir.is_dir() {
+    if destination.is_file() {
         return Err(format!(
-            "Directory {:?} does not exist.",
-            dest_dir.as_os_str()
+            "Chosen path is a file, please choose a directory: {}",
+            destination.as_os_str().to_string_lossy()
         ));
     }
 
-    if dest_dir.is_file() {
+    if !destination.is_dir() {
         return Err(format!(
-            "Directory specified is a file: {:?}",
-            dest_dir.as_os_str()
+            "Directory {} does not exist.",
+            destination.as_os_str().to_string_lossy()
         ));
     }
+
+    let image_format = match image_format {
+        Some(format) => Some(ImageFormat::from_extension(format).unwrap()),
+        None => name_expr.map(|expression| ImageFormat::from_path(expression).unwrap()),
+    };
 
     for (index, file) in files.iter().enumerate() {
-        let mut path: PathBuf = if let Some(filename) = file.file_name() {
-            dest_dir.join(filename).to_path_buf()
-        } else {
-            dest_dir.to_path_buf()
-        };
-
         let file_name = match name_expr {
-            Some(expr) => {
-                let expr_path = Path::new(expr);
-                if let Some(extension) = expr_path.extension() {
-                    let new_name =
-                        format!("{}_{}.{}", expr, index + 1, extension.to_str().unwrap());
-                    new_name
+            Some(expression) => {
+                format!("{expression}_{index}")
+            }
+            None => {
+                if let Some(filename) = file.file_name() {
+                    filename.to_string_lossy().to_string()
                 } else {
-                    return Err(format!(
-                        "File expression {} does not have an extension.",
-                        expr
-                    ));
+                    format!("image_{index}")
                 }
             }
-            None => path.file_name().unwrap().to_str().unwrap().to_string(),
         };
-        path.set_file_name(file_name);
 
-        paths.push(path);
+        let path = destination.to_path_buf();
+        let mut path = path.join(file_name);
+
+        let is_formatted = match image_format {
+            Some(image_format) => path.set_extension(image_format.extensions_str()[0]),
+            None => match file.extension() {
+                Some(extension) => path.set_extension(extension),
+                None => return Err("File does not have an extension?".into()),
+            },
+        };
+
+        if is_formatted {
+            paths.push(path);
+        } else {
+            return Err("Error formatting output file".into());
+        }
     }
-
     Ok(paths)
 }
+
