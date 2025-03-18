11c93b8969f9ffe03cc45e895c017ac973b476cc
Daru <mica.tui@proton.me>
2025-03-18 20:42:59 +0200

    refactor: init tests

---
+++ librimi/src/tests.rs
@@ -0,0 +1,56 @@
+use std::{
+    io::{BufWriter, Cursor},
+    sync::mpsc::channel,
+    time::{Duration, Instant},
+};
+
+use crate::image::randomize::Randomizer;
+use ::image::DynamicImage;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+
+#[test]
+fn mass_write() {
+    let mut images: Vec<DynamicImage> = Vec::new();
+
+    for _ in 1..10 {
+        let image = DynamicImage::new_luma_a16(1920, 1080);
+
+        image.randomize();
+
+        images.push(image);
+    }
+
+    let (tx, rx) = channel();
+    rayon::scope(move |s| {
+        let tx = tx.clone();
+
+        s.spawn(move |_| {
+            images.par_iter().for_each_with(tx, |tx, image| {
+                let mut writer = BufWriter::new(Cursor::new(Vec::new()));
+                let begin = Instant::now();
+                match image.write_to(&mut writer, ::image::ImageFormat::Avif) {
+                    Ok(_) => {
+                        let end = Instant::now().duration_since(begin);
+                        let dur = Duration::from(end);
+                        tx.send(format!(
+                            r#"
+                                Image written successfully!
+                                Took {} seconds
+                            "#,
+                            dur.as_secs()
+                        ))
+                        .unwrap_or_default();
+                    }
+                    Err(e) => {
+                        tx.send(format!("Image failed to write with error.\n{e:?}"))
+                            .unwrap_or_default();
+                    }
+                };
+            });
+        });
+
+        rx.iter().fuse().for_each(|result| {
+            println!("{result:?}");
+        });
+    });
+}
